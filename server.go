1cebfa3c9ab6153d57195553c210fe3644506953
garagaradev <garagaradev@proton.me>
2024-10-04 01:21:57 +0700

    changed Println() to Printf()

--- server.go
+++ server.go
@@ -21,7 +21,7 @@ func main(){
   }
   defer conn.Close()
 
-  fmt.Println("Listening on %s\n", address)
+  fmt.Printff("Listening on %s\n", address)
 
   //buffer for accepting data
   buffer := make([]byte, 1024)
